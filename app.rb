62861a760908b4f1c93f207499d212ac8740e002
13RuN <runemal13@gmail.com>
2020-12-02 15:23:07 +0500

    init database, make @db

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db 
+		@db = SQLite3::Database.new 'lepra.db'
+		@db.results_as_hash = true
+end
+
+before do
+		init_db
+end
 
 get '/' do
 		erb 'Hello!'
@@ -10,4 +20,9 @@ end
 
 get '/new' do
 		erb :new
+end
+
+post '/new' do
+		@content = params[:content]
+		erb "you tiped #{@content}"
 end
